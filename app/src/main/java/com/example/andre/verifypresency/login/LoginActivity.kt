be667b004d24d116974c308bc368f845f62afe95
Andrei Mares <andreimar@htss.ro>
2018-10-03 12:04:48 +0300

    Linked app with firebase. Finished layout for register actvity

---
+++ app/src/main/java/com/example/andre/verifypresency/login/LoginActivity.kt
@@ -0,0 +1,15 @@
+package com.example.andre.verifypresency.login
+
+import android.os.Bundle
+import com.example.andre.verifypresency.BaseActivity
+import com.example.andre.verifypresency.R
+
+class LoginActivity: BaseActivity(){
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_login)
+    }
+}
---
+++ app/src/main/java/com/example/andre/verifypresency/login/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.example.andre.verifypresency.login
+
+import android.os.Bundle
+import com.example.andre.verifypresency.BaseActivity
+
+class RegisterActivity: BaseActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
